ce9e532a2dab8018991ea20e2e07e258e4a64dd1
atomkhav <khav.saroeun@gmail.com>
2026-08-01 11:32:43 +0700

    add to application liquibase

--- Process.md
+++ Process.md
@@ -1,3 +1,9 @@
-# Video 4: JpaSpecific
+# Phone Shop Night
 
 run : time: 1:05 min
+
+- run generate liquibase
+```  
+mvn liquibase:generateChangeLog
+```
+
--- src/main/java/com/piseth/java/school/phoneshop_night/entity/Brand.java
+++ src/main/java/com/piseth/java/school/phoneshop_night/entity/Brand.java
@@ -10,6 +10,9 @@ import javax.persistence.*;
 public class Brand {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
     private Integer id;
+
+    @Column(name = "brand_name")
     private String name;
 }
